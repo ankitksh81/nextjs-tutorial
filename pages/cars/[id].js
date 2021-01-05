12d580bb36bda0bae94666a6069b41d455957792
ankitksh81 <ankitksh81@gmail.com>
2021-01-06 00:43:27 +0530

    added server side rendering func

--- pages/cars/[id].js
+++ pages/cars/[id].js
@@ -39,3 +39,16 @@ export async function getStaticPaths() {
         fallback: false
     };
 }
+
+// Server Side Rendering on calls not at build time.
+
+
+// export async function getServerSideProps({ params }) {
+    
+//     const req = await fetch(`http://localhost:3000/${params.id}.json`);
+//     const data = await req.json();
+
+//     return {
+//         props: { car: data },
+//     }
+// }
